71af3a3e46036706f1de177693e210b0d6ba208a
Aryh Sanches <116659114+AryhSanches@users.noreply.github.com>
2022-12-13 08:42:17 -0300

    BD update

--- conexao.php
+++ conexao.php
@@ -15,5 +15,5 @@
     else{
         echo ("not coneted");
     }
-
+ 
 ?>
--- config.php
+++ config.php
@@ -4,15 +4,19 @@ use LDAP\Result;
 
     include_once("conexao.php");
 
-    $nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_STRING);
-    $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
-    $senha = filter_input(INPUT_POST, 'senha', FILTER_SANITIZE_STRING);
-    $date = filter_input(INPUT_POST, 'nascimento', FILTER_SANITIZE_STRING);
-    echo "Nome:" .$nome."<br>";
-    echo "E-mail: $email <br>";
-    echo "Senha: $senha <br>";
-    echo "Nascimento: $date <br>";
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+    $date = $_POST['date'];
+
 
     $Result_user = "INSERT INTO user (nome, email, senha, nascimento) VALUES ('$nome', '$email', '$senha', '$date', NOW())";
-    mysqli_query($conn, $Result_user);
+    $save = mysqli_query($conn, $Result_user);
+
+    if($save):
+        echo "Nome:" .$nome."<br>";
+        echo "E-mail: $email <br>";
+        echo "Senha: $senha <br>";
+        echo "Nascimento: $date <br>";
+    endif;
 ?>
